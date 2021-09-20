abfded4831b045f2bc229388b331dbe841019cb2
edinugroho <edidwinugroho80@gmail.com>
2021-09-21 01:22:30 +0700

    Drop files from .gitignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,27 +7,30 @@ public class Main {
         int game_board[][]  = board.get_board();
 
         System.out.println("1st Generation");
+//        board.fill(3,4);
+//        board.fill(3,5);
+//        board.fill(3,6);
+//        board.fill(4,4);
+//        board.fill(5,4);
+//        board.fill(5,5);
+//        board.fill(5,6);
+//        board.fill(4,6);
         board.fill(3,4);
         board.fill(3,5);
         board.fill(3,6);
-        board.fill(4,4);
-        board.fill(5,4);
-        board.fill(5,5);
-        board.fill(5,6);
-        board.fill(4,6);
         board.print_board();
 
 
         Board board1 = new Board(10,10);
         int neighbor_board[][]  = board1.get_board();
 
-        System.out.println("Count Neighbor");
+//        System.out.println("Count Neighbor");
         for (int i = 0; i < board.get_x(); i++){
             for (int j = 0; j < board.get_y(); j++) {
                 neighbor_board[i][j] = board.count_neighbor(i,j);
             }
         }
-        board1.print_board();
+//        board1.print_board();
 
         Board board2 = new Board(10,10);
         int next_board[][]  = board2.get_board();
@@ -39,13 +42,5 @@ public class Main {
             }
         }
         board2.print_board();
-
-        System.out.println("Count Neighbor");
-        for (int i = 0; i < board.get_x(); i++){
-            for (int j = 0; j < board.get_y(); j++) {
-                neighbor_board[i][j] = board.count_neighbor(i,j);
-            }
-        }
-        board1.print_board();
     }
 }
